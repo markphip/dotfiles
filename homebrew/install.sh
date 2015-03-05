8c64553e0c590683920228f5cc9946d84c9bc1ea
Mark Phippard <mphippard@collab.net>
2015-03-05 17:11:50 -0500

    Update Homebrew install and add Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,16 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Here we go.. ask for the administrator password upfront and run a
+# keep-alive to update existing `sudo` time stamp until script has finished
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
 # Install homebrew packages
-brew install grc coreutils spark
+brew install coreutils grc
+brew install caskroom/cask/brew-cask
+brew tap Homebrew/brewdler
+brew cask install java
 
 exit 0
